2912d462b806e64cd71376a45668174d3bbccb41
Aleena-011 <aleenashd097@gmail.com>
2025-08-12 15:35:47 +0500

    input validation changes

--- routes/login.ts
+++ routes/login.ts
@@ -33,6 +33,16 @@ export function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
+    const { email, password } = req.body
+
+if (!email || !password) {
+  return res.status(400).send('Email and password are required')
+}
+
+if (!validator.isEmail(email)) {
+  return res.status(400).send('Invalid email format')
+}
+
     models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
       .then((authenticatedUser) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
