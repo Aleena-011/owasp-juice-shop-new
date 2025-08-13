e16c4f5be5168353138fd223ec2118c9f47f83fb
Aleena-011 <aleenashd097@gmail.com>
2025-08-13 16:12:01 +0500

    imported winston

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@
  * Copyright (c) 2014-2025 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
+import winston from 'winston'
 import i18n from 'i18n'
 import cors from 'cors'
 import fs from 'node:fs'
